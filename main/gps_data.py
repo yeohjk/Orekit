b7f3e596bffd117f32b8468d17b13eb53c9cb0d3
YeohJK <spin8053@gmail.com>
2025-06-12 18:49:43 +0800

    Added transformation of measurement data to PV object and created list of transformed data.

--- main/gps_data.py
+++ main/gps_data.py
@@ -4,16 +4,9 @@ import orekit
 from orekit.pyhelpers import setup_orekit_curdir
 from org.orekit.time import AbsoluteDate, TimeScalesFactory
 from org.orekit.frames import FramesFactory
-from org.orekit.bodies import OneAxisEllipsoid
-from org.orekit.utils import Constants, PVCoordinates, ParameterDriver
-from org.orekit.models.earth import ReferenceEllipsoid
-#from org.orekit.propagation.analytical.tle import TLE, TLEPropagatorBuilder
-from org.orekit.estimation.leastsquares import BatchLSEstimator
-from org.orekit.estimation.measurements import PV
-#from org.orekit.estimation.measurements.modifiers import OnBoardAntennaRangeModifier
-from org.orekit.estimation.measurements import ObservableSatellite
+from org.orekit.utils import PVCoordinates, IERSConventions
+from org.orekit.estimation.measurements import PV, ObservableSatellite
 from org.hipparchus.geometry.euclidean.threed import Vector3D
-from org.hipparchus.optim.nonlinear.vector.leastsquares import LevenbergMarquardtOptimizer
 
 # Initialises JDK and orekit library data
 orekit.initVM()
@@ -22,8 +15,11 @@ setup_orekit_curdir(from_pip_library=True)
 # Setting variables needed for data transformation
 utc = TimeScalesFactory.getUTC()
 inertial = FramesFactory.getEME2000()
-ecef = FramesFactory.getITRF(FramesFactory.getITRFType(IERSConventions.IERS_2010), True)
+ecef = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
+satellite = ObservableSatellite(0)
+list_measurements = []
 
+# Extracts, transforms and loads data
 with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
     file_content = csv.reader(file)
     next(file_content)
@@ -31,13 +27,17 @@ with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
         # Extracting and transforming date time
         datetime_str = row[0]
         datetime_obj = datetime.strptime(datetime_str, '%d/%m/%Y %H:%M:%S')
-        year = datetime_obj.year
-        month = datetime_obj.month
-        day = datetime_obj.day
-        hour = datetime_obj.hour
-        minute = datetime_obj.minute
-        second = float(datetime_obj.second)
+        year, month, day = datetime_obj.year, datetime_obj.month, datetime_obj.day
+        hour, minute, second = datetime_obj.hour, datetime_obj.minute, float(datetime_obj.second)
         abs_date = AbsoluteDate(year, month, day, hour, minute, second, utc)
-        print(abs_date)
-        # Extracting and transforming position
-        
+        # Extracting and transforming position and velocity from non-inertial ITRF ECEF frame to inertial EME2000 frame
+        pos_x, pos_y, pos_z = float(row[1]), float(row[2]), float(row[3])
+        position = Vector3D(pos_x, pos_y, pos_z)
+        vel_x, vel_y, vel_z = float(row[4]), float(row[5]), float(row[6])
+        velocity = Vector3D(vel_x, vel_y, vel_z)
+        pv_ecef = PVCoordinates(position, velocity)
+        transform = ecef.getTransformTo(inertial, abs_date)
+        pv_inertial = transform.transformPVCoordinates(pv_ecef)
+        # Loads transformed data to list
+        measurement = PV(abs_date, pv_inertial.getPosition(), pv_inertial.getVelocity(), 1.0, 1.0, 1.0, satellite)
+        list_measurements.append(measurement)
