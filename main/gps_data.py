6808e6039f6e9118637fa5aa298f48cdcf807e29
YeohJK <spin8053@gmail.com>
2025-06-12 16:28:49 +0800

    Added extraction and transformation of date time from python datetime object to absolute date object.

--- main/gps_data.py
+++ main/gps_data.py
@@ -25,6 +25,16 @@ with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
     file_content = csv.reader(file)
     next(file_content)
     for row in file_content:
+        # Extracting and transforming date time
         datetime_str = row[0]
         datetime_obj = datetime.strptime(datetime_str, '%d/%m/%Y %H:%M:%S')
-        print(datetime_obj)
+        year = datetime_obj.year
+        month = datetime_obj.month
+        day = datetime_obj.day
+        hour = datetime_obj.hour
+        minute = datetime_obj.minute
+        second = float(datetime_obj.second)
+        abs_date = AbsoluteDate(year, month, day, hour, minute, second, utc)
+        print(abs_date)
+        # Extracting and transforming position
+        
