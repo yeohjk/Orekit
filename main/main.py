7696f0562fc5ced136d0468267c51345d9fbebaa
YeohJK <spin8053@gmail.com>
2025-06-13 16:39:57 +0800

    Changed comment tense to better represent postconditions.

--- main/main.py
+++ main/main.py
@@ -17,7 +17,7 @@ from org.orekit.propagation.analytical.tle.generation import LeastSquaresTleGene
 orekit.initVM()
 setup_orekit_curdir(from_pip_library=True)
 
-# Setting variables needed for data transformation
+# Sets variables needed for data transformation
 utc = TimeScalesFactory.getUTC()
 inertial = FramesFactory.getEME2000()
 ecef = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
@@ -29,13 +29,13 @@ with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
     file_content = csv.reader(file)
     next(file_content)
     for row in file_content:
-        # Extracting and transforming date time
+        # Extractsand transforms date time
         datetime_str = row[0]
         datetime_obj = datetime.strptime(datetime_str, '%d/%m/%Y %H:%M:%S')
         year, month, day = datetime_obj.year, datetime_obj.month, datetime_obj.day
         hour, minute, second = datetime_obj.hour, datetime_obj.minute, float(datetime_obj.second)
         abs_date = AbsoluteDate(year, month, day, hour, minute, second, utc)
-        # Extracting and transforming position and velocity from non-inertial ITRF ECEF frame to inertial EME2000 frame
+        # Extracts and transforms position and velocity from non-inertial ITRF ECEF frame to inertial EME2000 frame
         pos_x, pos_y, pos_z = float(row[1]), float(row[2]), float(row[3])
         position = Vector3D(pos_x, pos_y, pos_z)
         vel_x, vel_y, vel_z = float(row[4]), float(row[5]), float(row[6])
@@ -47,7 +47,7 @@ with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
         measurement = PV(abs_date, pv_inertial.getPosition(), pv_inertial.getVelocity(), 1.0, 1.0, 1.0, satellite)
         list_measurements.append(measurement)
 
-# Creating TLE initial guess
+# Creates TLE initial guess
 with open("../../../TLE/TeLEOS-1/TLE TeLEOS-1 20250611 Spacetrack.txt", "r") as file:
     next(file)
     file_content = file.readlines()
@@ -56,8 +56,9 @@ with open("../../../TLE/TeLEOS-1/TLE TeLEOS-1 20250611 Spacetrack.txt", "r") as 
 tle_initial = TLE(tle_line1, tle_line2)
 print(tle_initial)
 
-# Creating TLE Propagator Builder
+# Creates TLE Propagator Builder
 builder = TLEPropagatorBuilder(tle_initial, PositionAngleType.TRUE, 1.0, LeastSquaresTleGenerationAlgorithm())
 
+# Creates optimizer and estimator
 
 
