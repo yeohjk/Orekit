2a7ef8560a0e988e0079c1958ba37dfee30db02e
YeohJK <spin8053@gmail.com>
2025-06-13 17:33:46 +0800

    Added estimation and generation of new TLE.

--- main/main.py
+++ main/main.py
@@ -8,10 +8,12 @@ from org.orekit.frames import FramesFactory
 from org.orekit.utils import PVCoordinates, IERSConventions
 from org.orekit.estimation.measurements import PV, ObservableSatellite
 from org.hipparchus.geometry.euclidean.threed import Vector3D
-from org.orekit.propagation.analytical.tle import TLE
+from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
 from org.orekit.propagation.conversion import TLEPropagatorBuilder
 from org.orekit.orbits import PositionAngleType
 from org.orekit.propagation.analytical.tle.generation import LeastSquaresTleGenerationAlgorithm
+from org.hipparchus.optim.nonlinear.vector.leastsquares import LevenbergMarquardtOptimizer
+from org.orekit.estimation.leastsquares import BatchLSEstimator
 
 # Initialises JVM and orekit library data
 orekit.initVM()
@@ -48,17 +50,30 @@ with open("data/WOD_GPS_Test_Data_ 100525.csv","r") as file:
         list_measurements.append(measurement)
 
 # Creates TLE initial guess
-with open("../../../TLE/TeLEOS-1/TLE TeLEOS-1 20250611 Spacetrack.txt", "r") as file:
+with open("../../../TLE/TeLEOS-1/TLE TeLEOS-1 20250509 Spacetrack.txt", "r") as file:
     next(file)
     file_content = file.readlines()
     tle_line1 = file_content[0][:-1]
     tle_line2 = file_content[1]
 tle_initial = TLE(tle_line1, tle_line2)
+print("Initial TLE")
 print(tle_initial)
 
 # Creates TLE Propagator Builder
 builder = TLEPropagatorBuilder(tle_initial, PositionAngleType.TRUE, 1.0, LeastSquaresTleGenerationAlgorithm())
 
 # Creates optimizer and estimator
+estimator = BatchLSEstimator(LevenbergMarquardtOptimizer(), builder)
+for measurement in list_measurements:
+    estimator.addMeasurement(measurement)
+estimator.setMaxIterations(100)
+estimator.setMaxEvaluations(1000)
+
+# Estimates new TLE based on measurement data
+propagator_estimated_tle = estimator.estimate()[0]
+tlepropagator_estimated_tle = TLEPropagator.cast_(propagator_estimated_tle).getTLE()
+print("Revised TLE")
+print(tlepropagator_estimated_tle)
+
 
 
