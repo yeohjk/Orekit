b5875a045922bd32f93b8cf146f0db9296b6ed48
YeohJK <spin8053@gmail.com>
2025-06-22 23:23:45 +0800

    Testing functionality for sheet selection, row and column checks.

--- main/WOD_data.py
+++ main/WOD_data.py
@@ -2,4 +2,6 @@
 import openpyxl 
 
 # Loads workbook
-wb = openpyxl.load_workbook('../../Data/TELEOS_1/WOD/WOD_SID_10_20May.xls')
+wb = openpyxl.load_workbook('../../Data/TELEOS_1/WOD/WOD_SID_10_20May.xlsx')
+ws = wb['0x80070000 - Data Order 1']
+print('Total number of rows: '+str(ws.max_row)+'. And total number of columns: '+str(ws.max_column))
