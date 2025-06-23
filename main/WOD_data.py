35ca08896e3d0fe6feb42358919e9033350e775c
YeohJK <spin8053@gmail.com>
2025-06-23 14:35:26 +0800

    Added list list_sheets for sheet names of PV data.

--- main/WOD_data.py
+++ main/WOD_data.py
@@ -1,6 +1,14 @@
 # Imports packages
 import openpyxl 
 
+# Sets up lists
+list_sheets = ["0x80070000 - Data Order 1",\
+               "0x80080000 - Data Order 1",\
+               "0x80090000 - Data Order 1",\
+               "0x800A0000 - Data Order 1",\
+               "0x800B0000 - Data Order 1",\
+               "0x800C0000 - Data Order 1",]
+
 # Loads workbook
 wb = openpyxl.load_workbook('../../Data/TELEOS_1/WOD/WOD_SID_10_20May.xlsx')
 ws = wb['0x80070000 - Data Order 1']
