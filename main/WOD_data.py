e2965db4f02bf01f96e2e5cb842d53c01dd74323
YeohJK <spin8053@gmail.com>
2025-06-24 18:07:46 +0800

    Added object oriented functionality using class ETL and. Transferred all script commands to be under the class ETL as methods and attributes. Added method of loading transformed data to csv file.

--- main/WOD_data.py
+++ main/WOD_data.py
@@ -1,40 +1,101 @@
 # Imports packages
 import openpyxl 
 from datetime import datetime
+import os
+import csv
 
-# Sets up dictionaries and lists
-dict_sheets = {"pos_x":"0x80070000 - Data Order 1",\
-               "pos_y":"0x80080000 - Data Order 1",\
-               "pos_z":"0x80090000 - Data Order 1",\
-               "vel_x":"0x800A0000 - Data Order 1",\
-               "vel_y":"0x800B0000 - Data Order 1",\
-               "vel_z":"0x800C0000 - Data Order 1"}
-dict_lists = {"datetime":[],\
-                "pos_x":[],\
-               "pos_y":[],\
-               "pos_z":[],\
-               "vel_x":[],\
-               "vel_y":[],\
-               "vel_z":[]}
+# Defines class ETL
+class ETL:
+    def __init__(self):
+        self.file_selector()
+        self.file_loader()
+        self.setup_dict()
+        self.ext_trans()
+        self.ext_trans_time()
+        self.load_to_csv()
+        return
+    def file_selector(self):
+        # Opens file directory to search for input WOD xlsx file
+        print(f"Files in Directory WOD")
+        for file_item in os.listdir("../../Data/TELEOS_1/WOD"):
+            print(file_item)
+        self.input_file_name = input(f"Input WOD Data file name with xlsx extension: ")
+        self.input_file_path = f"../../Data/TELEOS_1/WOD/{self.input_file_name}"
+        print("Input file path:", self.input_file_path)
+        return
+    def file_loader(self):
+        # Loads workbook
+        self.wb = openpyxl.load_workbook(self.input_file_path)
+    def setup_dict(self):
+        # Sets up dictionaries and lists
+        self.dict_sheets = {"pos_x":"0x80070000 - Data Order 1",\
+                            "pos_y":"0x80080000 - Data Order 1",\
+                            "pos_z":"0x80090000 - Data Order 1",\
+                            "vel_x":"0x800A0000 - Data Order 1",\
+                            "vel_y":"0x800B0000 - Data Order 1",\
+                            "vel_z":"0x800C0000 - Data Order 1"}
+        self.dict_lists = {"datetime":[],\
+                            "pos_x":[],\
+                            "pos_y":[],\
+                            "pos_z":[],\
+                            "vel_x":[],\
+                            "vel_y":[],\
+                            "vel_z":[]}
+        self.dict_trans = {"pos_x":self.pos_transform,\
+                            "pos_y":self.pos_transform,\
+                            "pos_z":self.pos_transform,\
+                            "vel_x":self.vel_transform,\
+                            "vel_y":self.vel_transform,\
+                            "vel_z":self.vel_transform }
+    # Transforms position data units of dm to m
+    def pos_transform(self, data_raw):
+        data_trans = data_raw/10
+        return data_trans
+    # Transforms velocity data units of mm/s to m/s
+    def vel_transform(self, data_raw):
+        data_trans = data_raw/1000
+        return data_trans
+    # Loops through sheets, extracts and transforms raw data to SI unit data
+    def ext_trans(self):
+        for field in self.dict_sheets:
+            trans_mtd = self.dict_trans[field]
+            self.ws = self.wb[self.dict_sheets[field]]
+            print(f"For Data {field}")
+            print('Total number of rows: '+str(self.ws.max_row)+'. And total number of columns: '+str(self.ws.max_column)+'\n')
+            for row_num in range(11, 2821+1):
+                val_raw = self.ws.cell(row=row_num, column=1).value
+                val_trans = trans_mtd(val_raw)
+                self.dict_lists[field].append(val_trans)
+        print("Extracted and transformed all PV data")
+        return
+    # Extracts and transforms datetime from input file 
+    def ext_trans_time(self):
+        self.ws = self.wb[self.dict_sheets["pos_x"]]
+        print("For Data datetime")
+        print('Total number of rows: '+str(self.ws.max_row)+'. And total number of columns: '+str(self.ws.max_column)+'\n')
+        for row_num in range(11, 2821+1):
+            val_raw = self.ws.cell(row=row_num, column=8).value
+            self.dict_lists["datetime"].append(val_raw)
+        self.num_datapoints = len(self.dict_lists["datetime"])
+        print("Extracted all datetime data")
+        return
+    # Loads data to output csv file
+    def load_to_csv(self):
+        with open("../../Data/TELEOS_1/WOD/WOD_GPS_Test_Data_PVT.csv", "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Time Stamp",\
+                             "X (m)",\
+                             "Y (m)",\
+                             "Z (m)",\
+                             "Vel X (m/s)",\
+                             "Vel Y (m/s)",\
+                             "Vel Z (m/s)"])
+            for ind in range(self.num_datapoints):
+                row_entry = []
+                for field in self.dict_lists:
+                    row_entry.append(self.dict_lists[field][ind])
+                writer.writerow(row_entry)
+        return
 
-# Transforms position data units of dm to m
-def pos_transform(data_raw):
-    data_trans = data_raw/10
-    return data_trans
-
-# Transforms velocity data units of mm/s to m/s
-def vel_transform(data_raw):
-    data_trans = data_raw/1000
-    return data_trans
-
-# Loads workbook
-wb = openpyxl.load_workbook('../../Data/TELEOS_1/WOD/WOD_SID_10_20May.xlsx')
-
-# Loops through sheets
-for field in dict_sheets:
-    ws = wb[dict_sheets[field]]
-    print(f"For {field}")
-    print('Total number of rows: '+str(ws.max_row)+'. And total number of columns: '+str(ws.max_column))
-    for row_num in range(11,ws.max_row+1):
-        dict_lists[field].append(ws.cell(row=row_num,column=1).value)
-    print(dict_lists[field])
+# Creates ETL object which executes ETL process
+test = ETL()
